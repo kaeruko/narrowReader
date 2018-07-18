ae6a9f9510fb06557e1d7dbc7abd0537f3aa32de
kaeruko <mail@cloxs.jp>
2018-07-18 18:06:39 +0900

    zenbu mieru

--- narrowReader/NovelTableViewCell.swift
+++ narrowReader/NovelTableViewCell.swift
@@ -12,7 +12,7 @@ class NovelTableViewCell: UITableViewCell {
     
     var title: UILabel!
     
-    var story: UILabel!
+    var summary: UILabel!
     
     open func createUIView(uiview : UIView) -> UIView{
         uiview.translatesAutoresizingMaskIntoConstraints = false
@@ -20,7 +20,7 @@ class NovelTableViewCell: UITableViewCell {
         return uiview
     }
     
-
+    
     open func createLabel() -> UILabel{
         var label = UILabel(frame:CGRect(x: 10, y: 10, width: self.frame.width * 0.8 , height: 30))
         label = self.createUIView(uiview: label) as! UILabel
@@ -45,33 +45,33 @@ class NovelTableViewCell: UITableViewCell {
         ret.priority = priority
         return ret
     }
-
+    
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        self.title = self.createLabel()
-        self.story = self.createLabel()
-//        self.title.frame(forAlignmentRect: CGRect(x: 0, y: 0, width: self.frame.width * 0.9 , height: 0))
-        self.story.frame(forAlignmentRect: CGRect(x: 0, y: 0, width: self.frame.width, height: 0))
-//        self.title.numberOfLines = 0
-        self.story.numberOfLines = 0
-//        self.title.adjustsFontSizeToFitWidth = true
-        self.story.adjustsFontSizeToFitWidth = true
-//        self.story.font = UIFont.systemFont(ofSize: 12)
-//        self.layoutElement(target: self, element: self.title, attr: NSLayoutAttribute.top, constant: 2)
-//        self.layoutElement(target: self, element: self.title, attr: NSLayoutAttribute.leading, constant: 2)
-//        self.layoutElement(target: self, element: self.story, attr: NSLayoutAttribute.centerX, constant: 0)
-
-//        self.layoutElement(target: self, element: self.story, attr: NSLayoutAttribute.top, constant: 30)
-//        self.layoutElement(target: self, element: self.story, attr: NSLayoutAttribute.leading, constant: 2)
-//        self.layoutElement(target: self, element: self.story, attr: NSLayoutAttribute.top, constant: 30)
-
+        //        self.title = self.createLabel()
+        self.summary = self.createLabel()
+        //        self.title.frame(forAlignmentRect: CGRect(x: 0, y: 0, width: self.frame.width * 0.9 , height: 0))
+        self.summary.frame(forAlignmentRect: CGRect(x: 0, y: 0, width: self.frame.width*1.5, height: 0))
+        self.summary.font = UIFont.systemFont(ofSize: 12)
+        //        self.title.adjustsFontSizeToFitWidth = true
+        //        self.summary.adjustsFontSizeToFitWidth = true
+        //        self.title.numberOfLines = 0
+        //        self.summary.numberOfLines = 0
+        //        self.layoutElement(target: self, element: self.title, attr: NSLayoutAttribute.top, constant: 2)
+        //        self.layoutElement(target: self, element: self.title, attr: NSLayoutAttribute.leading, constant: 2)
+        //        self.layoutElement(target: self, element: self.summary, attr: NSLayoutAttribute.centerX, constant: 0)
+        
+        //        self.layoutElement(target: self, element: self.summary, attr: NSLayoutAttribute.top, constant: 30)
+                self.layoutElement(target: self, element: self.summary, attr: NSLayoutAttribute.leading, constant: 2)
+        //        self.layoutElement(target: self, element: self.summary, attr: NSLayoutAttribute.top, constant: 30)
+        
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-        
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
--- narrowReader/viewControllers/SearchResultViewController.swift
+++ narrowReader/viewControllers/SearchResultViewController.swift
@@ -11,8 +11,8 @@ import Alamofire
 import RealmSwift
 
 class SearchResultViewController: narrowPageViewController,  UITableViewDelegate, UITableViewDataSource {
-
-
+    
+    
     var nnumber:Int?
     var search_word:String?
     var novelcount : Int = 0
@@ -25,26 +25,34 @@ class SearchResultViewController: narrowPageViewController,  UITableViewDelegate
         self.view.backgroundColor = UIColor.white
         self.searchByApi()
         print(self.search_word)
-        
-        
-        let myTableView = UITableView(frame: view.frame, style: .plain)
-        myTableView.estimatedRowHeight = UITableViewAutomaticDimension
-        myTableView.rowHeight = UITableViewAutomaticDimension
-        myTableView.delegate      =   self as UITableViewDelegate
-        myTableView.dataSource    =   self as! UITableViewDataSource
-        myTableView.register(NovelTableViewCell.self, forCellReuseIdentifier: NSStringFromClass(NovelTableViewCell.self))
-        self.view.addSubview(myTableView)
+    }
+    
+    private var cellHeightList: [IndexPath: CGFloat] = [:]
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
         
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.novelcount
     }
+
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(NovelTableViewCell.self), for: indexPath) as! NovelTableViewCell
+        cell.summary?.font = UIFont.systemFont(ofSize: 12)
+        cell.summary?.minimumScaleFactor = 15.0/15.0;
+        cell.summary?.adjustsFontSizeToFitWidth = true
+        cell.summary?.numberOfLines = 0
+        cell.summary?.text = self.results?[indexPath.row]["story"] as? String
         cell.title?.text = self.results?[indexPath.row]["title"] as? String
-        cell.story?.text = self.results?[indexPath.row]["story"] as? String
         cell.layoutIfNeeded()
         return cell
     }
@@ -58,77 +66,79 @@ class SearchResultViewController: narrowPageViewController,  UITableViewDelegate
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
     open func searchByApi() {
         
-        let file_name = "search.txt"
+//        let file_name = "search.txt"
+//
+//        if let dir = FileManager.default.urls( for: .documentDirectory, in: .userDomainMask ).first {
+//            //print(dir)
+//            let path_file_name = dir.appendingPathComponent( file_name )
+//            do {
+//
+//                let searchresult: String = try String( contentsOf: path_file_name, encoding: String.Encoding.utf8 )
+//                //                print(searchresult)
+//                var sr: Data =  searchresult.data(using: String.Encoding.utf8)!
+//
+//                // パースする
+//                let items:NSArray = try JSONSerialization.jsonObject(with: sr, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
+//                //                    print(items[1])
+//                for n in items {
+//                    let i = n as! Dictionary<String,Any?>
+//                    if((i["title"] == nil)){continue}
+//                    self.results?.append(["title": i["title"], "ncode" : i["ncode"], "general_all_no":i["general_all_no"], "novel_type": i["novel_type"],"story": i["story"] ])
+//
+//                }
+//                self.novelcount = (self.results?.count)!
+//
+//            } catch {
+//                //エラー処理
+//            }
+//        }
         
-        if let dir = FileManager.default.urls( for: .documentDirectory, in: .userDomainMask ).first {
-print(dir)
-            let path_file_name = dir.appendingPathComponent( file_name )
-            do {
-                
-                let searchresult: String = try String( contentsOf: path_file_name, encoding: String.Encoding.utf8 )
-                print(searchresult)
-                var sr: Data =  searchresult.data(using: String.Encoding.utf8)!
-                
-                // パースする
-                let items:NSArray = try JSONSerialization.jsonObject(with: sr, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
-                //                    print(items[1])
-                for n in items {
-                    let i = n as! Dictionary<String,Any?>
-                    if((i["title"] == nil)){continue}
-                    self.results?.append(["title": i["title"], "ncode" : i["ncode"], "general_all_no":i["general_all_no"], "novel_type": i["novel_type"],"story": i["story"] ])
-                    
-                }
-                self.novelcount = (self.results?.count)!
-                
-            } catch {
-                //エラー処理
-            }
-        }
+//        for n in self.results! {
+//            let i = n as! Dictionary<String,Any?>
+//            //            print(i)
+//            //            print(i["title"])
+//            print(i["story"])
+//        }
         
-        for n in self.results! {
-            let i = n as! Dictionary<String,Any?>
-            print(i)
-            print(i["title"])
-            print(i["story"])
-        }
-        
-        return;
         
         //https://api.syosetu.com/novel18api/api/?libtype=1&out=json&word=%E7%9B%A3%E7%A6%81
         //https://novel18.syosetu.com/txtdownload/dlstart/ncode/1250059/?no=1&hankaku=0&code=utf-8&kaigyo=crlf
-        Alamofire.request("https://api.syosetu.com/novel18api/api/?libtype=1&out=json&word=%E7%9B%A3%E7%A6%81", headers:["Cookie": "over18=yes;"]).response { response in      //連載
+        Alamofire.request("https://api.syosetu.com/novel18api/api/?libtype=1&out=json&nocgenre=2&word=%E7%9B%A3%E7%A6%81", headers:["Cookie": "over18=yes;"]).response { response in      //連載
             
             
             if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                //                print("Data: \(utf8Text)") // original server data as UTF8 string
                 
                 let regex = try! NSRegularExpression(pattern: "S(w+)ift", options: [])
                 var searchresult: Data =  utf8Text.data(using: String.Encoding.utf8)!
                 do {
                     // パースする
                     let items:NSArray = try JSONSerialization.jsonObject(with: searchresult, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
-                    //                    print(items[1])
-                    let i = items[1] as! Dictionary<String,Any?>
-                    //                    let i = items[1] as! Dictionary<Int, [String:String]?>
-                    print(i["general_all_no"])
-                    
-                    
-                    //                    let items:Dictionary<Int, Any?> = try JSONSerialization.jsonObject(with: personalData, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<Int, Any?>
-                    //                    let i = items[0] as! [String:String]
-                    //                    let location = users[0]["user_location"] ?? "(unknown)"
-                    //                    print(i)
-                    //
-                    //                    print(items[1]["keyword"]) // メンバid Intにキャスト
-                    //
-                    //                    if let i = items.value as? [String:[String]]
-                    //                    {
-                    //                        print(i[1]["keyword"]) // メンバid Intにキャスト
-                    //                    }
-                    //                    print(items["name"] as! String) // メンバname Stringにキャスト
+
+                    for n in items {
+                        let i = n as! Dictionary<String,Any?>
+                        print(i["story"])
+
+                        if((i["title"] == nil)){continue}
+                        self.results?.append(["title": i["title"], "ncode" : i["ncode"], "general_all_no":i["general_all_no"], "novel_type": i["novel_type"],"story": i["story"] ])
+    
+                    }
+                    self.novelcount = (self.results?.count)!
+
+
+                    let myTableView = UITableView(frame: self.view.frame, style: .plain)
+                    myTableView.estimatedRowHeight = UITableViewAutomaticDimension
+                    myTableView.rowHeight = UITableViewAutomaticDimension
+                    myTableView.flashScrollIndicators()
+                    myTableView.delegate      =   self as UITableViewDelegate
+                    myTableView.dataSource    =   self as! UITableViewDataSource
+                    myTableView.register(NovelTableViewCell.self, forCellReuseIdentifier: NSStringFromClass(NovelTableViewCell.self))
+                    self.view.addSubview(myTableView)
+
                 } catch {
                     print(error)
                 }
@@ -136,5 +146,5 @@ print(dir)
         }
     }
     
-
+    
 }
