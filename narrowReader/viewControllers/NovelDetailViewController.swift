e81e4ae1fbef3a0fcc5d8d59fe30ff7687c95664
kaeruko <mail@cloxs.jp>
2018-07-21 15:35:26 +0900

    honbun syutoku

--- narrowReader/viewControllers/NovelDetailViewController.swift
+++ narrowReader/viewControllers/NovelDetailViewController.swift
@@ -55,10 +55,11 @@ class NovelDetailViewController: narrowBaseViewController, UIScrollViewDelegate 
         self.scrollView.frame.size = CGSize(width: self.frameWidth * 1.0 , height: self.frameHeight * 1.0 )
         self.scrollView.center = self.view.center
         self.textLabel.frame = CGRect(x: 0 , y: 0, width: self.frameWidth * 1.0, height: self.frameHeight * 1.0)
-        
+        self.textLabel.text = "お待ち下さい"
         self.scrollView.addSubview(self.textLabel)
         // Delegate を設定
         self.scrollView.delegate = self as! UIScrollViewDelegate
+        self.view.addSubview(scrollView)
     }
 
     func getNovelNumber(){
@@ -67,13 +68,15 @@ class NovelDetailViewController: narrowBaseViewController, UIScrollViewDelegate 
         url.append("/")
         Alamofire.request(url, headers:["Cookie": "over18=yes;"]).response { response in
             if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+print(utf8Text)
                 var ans: [String] = []
                 utf8Text.pregMatche(pattern: "ncode/(\\d+)/", matches: &ans)
                 print(ans[1])
                 do {
-                    
+                    self.textLabel.text = "本文取得準備中"
                     self.getNovelText(nnumber: Int(ans[1])!, no: 1)
                 } catch {
+print("失敗")
                     print(error)
                 }
             }
@@ -81,28 +84,41 @@ class NovelDetailViewController: narrowBaseViewController, UIScrollViewDelegate 
 
     }
 
-    func getNovelText(nnumber : Int , no : Int = 0){
+    var noveltext : String = ""
+    func getNovelText(nnumber : Int , no : Int = 0) {
+print(self.noveltext)
         var url : String = "https://novel18.syosetu.com/txtdownload/dlstart/ncode/"
         url.append(String(nnumber ))
         url.append("/?no=")
         url.append(String(no))
         url.append("&hankaku=0&code=utf-8&kaigyo=crlf")
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        print(url)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
             Alamofire.request(url, headers:["Cookie": "over18=yes;"]).response { response in
                 if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
                     do {
-                        
-                        self.getNovelText(nnumber : nnumber, no : no+1)
+                        var waitingtext : String = "本文取得中……("
+                        waitingtext.append(String(no))
+                        waitingtext.append(" / ")
+                        waitingtext.append(String(self.ndetail.general_all_no))
+                        waitingtext.append(")")
+                        self.textLabel.text = waitingtext
+                        self.noveltext.append("\n\(no)\n")
+                        self.noveltext.append(utf8Text)
+print("self.ndetail.general_all_no")
+print(String(self.ndetail.general_all_no)+" : "+String(no))
+                        if(no >= self.ndetail.general_all_no){
+                            self.textLabel.text = self.noveltext
+                            return
+                        }
+                        self.getNovelText(nnumber: nnumber,no:no+1)
                     } catch {
                         print(error)
                     }
                 }
             }
-            
-
         }
-
     }
     
     func getNovel(){
